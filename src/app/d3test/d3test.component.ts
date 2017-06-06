eafb8123b90acd1c0e1c186ef42d66b784eba68c
bersling <bersling@gmail.com>
2017-06-06 18:13:27 +0200

    increase distance a wee bit

--- src/app/d3test/d3test.component.ts
+++ src/app/d3test/d3test.component.ts
@@ -9,7 +9,7 @@ const radarChart = function RadarChart(id, data, options) {
     margin: {top: 20, right: 20, bottom: 20, left: 20}, // The margins of the SVG
     levels: 3,				// How many levels or inner circles should there be drawn
     maxValue: 0, 			// What is the value that the biggest circle will represent
-    labelFactor: 1.1, 	// How much farther than the radius of the outer circle should the labels be placed
+    labelFactor: 1.2, 	// How much farther than the radius of the outer circle should the labels be placed
     wrapWidth: 60, 		// The number of pixels after which a label needs to be given a new line
     opacityArea: 0.35, 	// The opacity of the area of the blob
     dotRadius: 4, 			// The size of the colored circles of each blog
