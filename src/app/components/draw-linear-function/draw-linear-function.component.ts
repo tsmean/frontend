aad31cf6f18e766aab0cfbe9ba6d804b45342538
bersling <bersling@gmail.com>
2017-06-13 14:54:11 +0200

    configurable component

--- src/app/components/draw-linear-function/draw-linear-function.component.ts
+++ src/app/components/draw-linear-function/draw-linear-function.component.ts
@@ -2,19 +2,6 @@ import {Component, ElementRef, Input, OnChanges, OnInit} from '@angular/core';
 
 import * as d3 from 'd3';
 
-interface LinearFunction {
-  slope?: number;
-  offset?: number;
-  error?: 'isPoint' | 'infiniteSlope';
-}
-
-interface Point {
-  x: number;
-  y: number;
-}
-
-type Slope = number | 'isPoint' | 'infiniteSlope';
-
 @Component({
   selector: 'app-draw-linear-function',
   templateUrl: './draw-linear-function.component.html',
@@ -22,80 +9,78 @@ type Slope = number | 'isPoint' | 'infiniteSlope';
 })
 export class DrawLinearFunctionComponent implements OnChanges {
 
-  // TODO's:
-  // redraw
-  // point should be over line
-
   @Input()
   width; // depends on rendered element, thus initialization code is in OnInit
-  height;
-  padding = 10;
-  xMax = 5;
-  xMin = -5;
-  xScale;
-  yScale;
-  radius = 6;
+
+  @Input()
+  xMax;
+
+  @Input()
+  xMin;
+
+  @Input()
+  circleRadius;
 
   dataset = [];
-
-  drag = this.setupDrag();
-
+  drag;
   svg;
+  padding = 10;
+  height;
 
+  circleAttrs (radius, xScale, yScale) {
+    return {
+      cx: function(d) { return xScale(d.x); },
+      cy: function(d) { return yScale(d.y); },
+      r: radius
+    };
+  }
 
   constructor(
     private el: ElementRef
   ) { }
 
   ngOnChanges() {
+    const plotConfig: PlotConfig = new PlotConfig(
+      this.width,
+      this.xMax,
+      this.xMin,
+      this.padding,
+      this.circleRadius
+    );
+    this.setup(plotConfig);
+  }
 
-    // on every change of date (currently only width on browser resize), redraw the chart
-
+  setupClick(svg, plotConfig: PlotConfig, drag) {
     const that = this;
-
-    that.height = that.width; // must be square
-    that.xScale = d3.scale.linear().domain([that.xMax, that.xMin]).range([that.width - that.padding, that.padding]);
-    that.yScale = d3.scale.linear().domain([that.xMin, that.xMax]).range([that.height - that.padding, that.padding]);
-
-    that.setup();
-
-    // On Click, we want to add data to the array and chart
-    that.svg.on('click', function() {
-      const coords = d3.mouse(this);
-
-      // Normally we go from data to pixels, but here we're doing pixels to data
-      const newData = {
-        x: Math.round( that.xScale.invert(coords[0])),  // Takes the pixel number to convert to number
-        y: Math.round( that.yScale.invert(coords[1]))
-      };
+    svg.on('click', function() {
 
       if (Object.keys(that.dataset).length < 2) {
+        const coords = d3.mouse(this);
+
+        // Normally we go from data to pixels, but here we're doing pixels to data
+        const newData = {
+          x: Math.round(plotConfig.xScale.invert(coords[0])),  // Takes the pixel number to convert to number
+          y: Math.round(plotConfig.yScale.invert(coords[1]))
+        };
+
         that.dataset.push(newData);   // Push data to our array
       }
 
-      const circleAttrs = {
-        cx: function(d) { return that.xScale(d.x); },
-        cy: function(d) { return that.yScale(d.y); },
-        r: that.radius
-      };
-
-      that.svg.selectAll('circle')  // For new circle, go through the update process
+      svg.selectAll('circle')  // For new circle, go through the update process
         .data(that.dataset)
         .enter()
         .append('circle')
-        .attr(circleAttrs)
-        .call(that.drag)
+        .attr(that.circleAttrs(plotConfig.radius, plotConfig.xScale, plotConfig.yScale))
+        .call(drag)
         .classed('circle', true);
 
-      that.drawLine();
+      that.drawLine(plotConfig);
 
     });
-
   }
 
-  setupDrag() {
-
-    const that = this;
+  // Todo: is this the correct return type?
+  generateDragBehaviour(xScale, yScale): d3.behavior.Drag<any> {
 
     const drag = d3.behavior.drag();
 
@@ -107,8 +92,8 @@ export class DrawLinearFunctionComponent implements OnChanges {
       d3.select(this).attr('transform', function(innerD, innerI){
         // duplication
         const newData = {
-          x: Math.round( that.xScale.invert(d.x)),  // Takes the pixel number to convert to number
-          y: Math.round( that.yScale.invert(d.y))
+          x: Math.round( xScale.invert(d.x)),  // Takes the pixel number to convert to number
+          y: Math.round( yScale.invert(d.y))
         };
 
         return 'translate(' + [ newData.x, newData.y ] + ')';
@@ -122,49 +107,56 @@ export class DrawLinearFunctionComponent implements OnChanges {
     return drag;
   }
 
-  setup() {
+  setup(plotConfig: PlotConfig) {
 
     const that = this;
 
-    // create an (new) that.svg container
+    console.log(plotConfig);
+
+    // create a new svg
     if (that.svg) {
       that.svg.remove();
     }
-    that.svg = d3.select('#graph')
-      .append('svg:svg')
-      .attr('width', that.width)
-      .attr('height', that.height);
+    const svg = that.svg = d3.select('#graph')
+      .append('svg:svg');
+
+    svg.attr('width', plotConfig.width)
+      .attr('height', plotConfig.height);
 
     // plot cartesian
     const yAxis = d3.svg.axis()
       .orient('left')
-      .scale(that.yScale);
+      .scale(plotConfig.yScale);
 
     // define the y axis
     const xAxis = d3.svg.axis()
       .orient('bottom')
-      .scale(that.xScale);
+      .scale(plotConfig.xScale);
 
     const xAxisPlot = that.svg.append('g')
       .attr('class', 'axis axis-x')
-      .attr('transform', 'translate(0,' + (that.height / 2) + ')')
+      .attr('transform', 'translate(0,' + (plotConfig.height / 2) + ')')
       .call(xAxis);
 
     const yAxisPlot = that.svg.append('g')
       .attr('class', 'axis axis-y')
-      .attr('transform', 'translate(' + (that.width / 2) + ',0)')
+      .attr('transform', 'translate(' + (plotConfig.width / 2) + ',0)')
       .call(yAxis);
 
     xAxisPlot.selectAll('.tick line')
-      .attr('y1', (that.width - (2 * that.padding)) / 2 * -1)
-      .attr('y2', (that.width - (2 * that.padding)) / 2 * 1);
+      .attr('y1', (plotConfig.width - (2 * plotConfig.padding)) / 2 * -1)
+      .attr('y2', (plotConfig.width - (2 * plotConfig.padding)) / 2 * 1);
 
     yAxisPlot.selectAll('.tick line')
-      .attr('x1', (that.width - (2 * that.padding)) / 2 * -1)
-      .attr('x2', (that.width - (2 * that.padding)) / 2 * 1);
+      .attr('x1', (plotConfig.width - (2 * plotConfig.padding)) / 2 * -1)
+      .attr('x2', (plotConfig.width - (2 * plotConfig.padding)) / 2 * 1);
+
+    const dragBehaviour = this.generateDragBehaviour(plotConfig.xScale, plotConfig.yScale);
+    this.setupClick(svg, plotConfig, dragBehaviour);
+    that.drawLine(plotConfig);
   }
 
-  drawLine() {
+  drawLine(plotConfig: PlotConfig) {
     const that = this;
     if (Object.keys(that.dataset).length === 2) {
 
@@ -174,22 +166,22 @@ export class DrawLinearFunctionComponent implements OnChanges {
       let myLine;
 
       if (typeof slope === 'number') {
-        const minY = that.linearFunction(that.xMin, slope, offset);
-        const maxY = that.linearFunction(that.xMax, slope, offset);
+        const minY = that.linearFunction(plotConfig.xMin, slope, offset);
+        const maxY = that.linearFunction(plotConfig.xMax, slope, offset);
 
         myLine = that.svg.append('line')          // attach a line
-          .attr('x1', that.xScale(that.xMin))     // x position of the first end of the line
-          .attr('y1', that.yScale(minY))      // y position of the first end of the line
-          .attr('x2', that.xScale(that.xMax))     // x position of the second end of the line
-          .attr('y2', that.yScale(maxY))    // y position of the second end of the line
+          .attr('x1', plotConfig.xScale(plotConfig.xMin))     // x position of the first end of the line
+          .attr('y1', plotConfig.yScale(minY))      // y position of the first end of the line
+          .attr('x2', plotConfig.xScale(plotConfig.xMax))     // x position of the second end of the line
+          .attr('y2', plotConfig.yScale(maxY))    // y position of the second end of the line
           .style('stroke-width', 2)
           .classed('connection-line', true);
       } else if (slope === 'infiniteSlope') {
         myLine = that.svg.append('line')          // attach a line
-          .attr('x1', that.xScale(that.dataset[0].x))     // x position of the first end of the line
-          .attr('y1', that.yScale(that.xMin))      // y position of the first end of the line
-          .attr('x2', that.xScale(that.dataset[0].x))     // x position of the second end of the line
-          .attr('y2', that.yScale(that.xMax))    // y position of the second end of the line
+          .attr('x1', plotConfig.xScale(that.dataset[0].x))     // x position of the first end of the line
+          .attr('y1', plotConfig.yScale(plotConfig.xMin))      // y position of the first end of the line
+          .attr('x2', plotConfig.xScale(that.dataset[0].x))     // x position of the second end of the line
+          .attr('y2', plotConfig.yScale(plotConfig.xMax))    // y position of the second end of the line
           .style('stroke-width', 2)
           .classed('connection-line', true);
       }
@@ -222,3 +214,36 @@ export class DrawLinearFunctionComponent implements OnChanges {
 
 
 }
+
+class PlotConfig {
+
+  xScale;
+  yScale;
+  height;
+
+  constructor(
+    public width,
+    public xMax,
+    public xMin,
+    public padding,
+    public radius
+  ) {
+    this.height = this.width;
+    this.xScale = d3.scale.linear().domain([xMax, xMin]).range([width - padding, padding]);
+    this.yScale = d3.scale.linear().domain([xMin, xMax]).range([this.height - padding, padding]);
+  }
+
+}
+
+interface LinearFunction {
+  slope?: number;
+  offset?: number;
+  error?: 'isPoint' | 'infiniteSlope';
+}
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+type Slope = number | 'isPoint' | 'infiniteSlope';
