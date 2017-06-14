2c6d6f54dbfc6887dd72e4146624414e6c4706a6
bersling <bersling@gmail.com>
2017-06-15 01:35:26 +0200

    update

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+server="ubuntu@54.93.232.18"
+
+echo "Bundle Angular"
+ng build --prod
+
+echo "Remove old directory"
+ssh ${server} "rm -rf tsmean/fe"
+
+echo "Upload new contents"
+ssh ${server} "mkdir tsmean/fe"
+scp -r dist "${server}:~/tsmean/fe/dist"
+scp server.js "${server}:~/tsmean/fe/server.js"
+scp -r package.json "${server}:~/tsmean/fe/package.json"
+
+echo "Install packages on server"
+ssh ${server} "cd tsmean/fe && npm install --production"
+
+echo "(Re-)Start server"
+ssh ${server} "forever stop tsmean/fe/server.js"
+ssh ${server} "forever start tsmean/fe/server.js"
+
+echo "Done!"
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 import {Environment} from './environment.model';
 export const environment: Environment = {
   production: true,
-  api: 'http://http://54.93.232.18:3000/api/v1/'
+  api: 'http://54.93.232.18:3000/api/v1/'
 };
