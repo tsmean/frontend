928972659b146e79b3eaee337e2740739090dcd3
bersling <bersling@gmail.com>
2017-07-24 01:21:28 +0200

    trial of new installation process

---
+++ install.ts
@@ -0,0 +1,65 @@
+import * as process from 'process';
+import {spawn, spawnSync, SpawnSyncOptionsWithStringEncoding, SpawnSyncReturns} from 'child_process';
+
+// declaring utf8 makes sure a string is returned (as opposed to a buffer) from spawnSync
+const spawnSyncOptions: SpawnSyncOptionsWithStringEncoding = {
+  encoding: 'utf8'
+};
+
+/**
+ * Some few adaptions are necessary for making installation work on windows
+ */
+const isWin = /^win/.test(process.platform);
+const spawnSyncCommand = (cmdName): string => {
+  return isWin ? `${cmdName}.cmd` : cmdName;
+};
+
+/**
+ * Setup git submodules
+ */
+const initSubmodules = spawnSync('git', ['submodule', 'init'], spawnSyncOptions);
+handleCommandResult(initSubmodules, {exitOnError: true});
+const updateSubmodules = spawnSync('git', ['submodule', 'update'], spawnSyncOptions);
+handleCommandResult(updateSubmodules, {exitOnError: false}); // git writes to stderr even though everything is ok, so noexit
+
+/**
+ * Install main module
+ */
+const startingDirectory = process.cwd();
+changeToDirectory(startingDirectory);
+changeToDirectory('main');
+const installModuleDependencies = spawnSync(spawnSyncCommand('npm'), ['install'], spawnSyncOptions);
+handleCommandResult(installModuleDependencies, {exitOnError: true});
+
+/**
+ * Helper functions
+ */
+function handleCommandResult(result: SpawnSyncReturns<string>, options?: HandleCommandResultOptions) {
+  if (result.error) {
+    console.error('ERROR in process:', result.error);
+    if (options.exitOnError) {
+      process.exit()
+    }
+  } else if (result.stderr !== undefined && result.stderr !== "") {
+    console.error('stderr not empty:', result.stderr);
+    if (options.exitOnError) {
+      process.exit()
+    }
+  } else {
+    console.log(result.stdout);
+  }
+}
+
+function changeToDirectory (dir) {
+  try {
+    console.log(`Changing to directory ${dir}`);
+    process.chdir(dir);
+  } catch (err) {
+    console.log(`Could not change to directory ${dir}: ${err}`);
+    process.exit();
+  }
+}
+
+interface HandleCommandResultOptions {
+  exitOnError: boolean
+}
